160fca8e665bb79de83fc09323df9308d83d44fc
connerdouglass <conner.douglass@gmail.com>
2021-08-12 03:13:51 -0700

    Added more test cases

--- parse_test.go
+++ parse_test.go
@@ -7,21 +7,20 @@ import (
 	"github.com/spiretechnology/go-timecode"
 )
 
-func TestParse_Zero(t *testing.T) {
-	tc, _ := timecode.Parse("00:00:00:00", timecode.Rate_23_976)
-	if tc.Frame() != 0 {
-		t.Error("Timecode 00:00:00:00 should be equivalent to frame 0")
-	} else {
-		t.Log("Success, timecode 00:00:00:00 equals frame 0")
+func TestParse_NDF(t *testing.T) {
+	cases := map[string]int64{
+		"00:00:00:00": 0,
+		"00:00:00:01": 1,
+		"00:00:01:01": 61,
+		"00:00:11:01": 661,
 	}
-}
-
-func TestParse_One(t *testing.T) {
-	tc, _ := timecode.Parse("00:00:00:01", timecode.Rate_23_976)
-	if tc.Frame() != 1 {
-		t.Error("Timecode 00:00:00:01 should be equivalent to frame 1")
-	} else {
-		t.Log("Success, timecode 00:00:00:01 equals frame 1")
+	for k, f := range cases {
+		tc, _ := timecode.Parse(k, timecode.Rate_60)
+		if frame := tc.Frame(); frame != f {
+			t.Errorf("Timecode %s should be equivalent to frame %d. Got %d\n", k, f, frame)
+		} else {
+			t.Logf("Success, timecode %s equals frame %d\n", k, f)
+		}
 	}
 }
 
